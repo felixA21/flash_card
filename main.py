e1c952e5cc3086cc19a9c76928f9ba415785a28d
Felix Alimin <felixalimin@gmail.com>
2024-05-28 21:54:01 -0400

    set up timer to change the background and display english word after 3 seconds

--- main.py
+++ main.py
@@ -3,25 +3,39 @@ from tkinter import *
 import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-
-# ------------------------ next card function --------------------------------
-
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
-def next_card():
-    current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_text, text=current_card["French"])
+current_card = {}
 
+# ------------------------ next card function --------------------------------
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill= "black")
+    canvas.itemconfig(card_text, text=current_card["French"], fill= "black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+# ------------------------ flip card function --------------------------------
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_text, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+# ------------------------ UI --------------------------------
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, flip_card)
+
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_text = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
